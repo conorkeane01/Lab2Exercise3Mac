bc188f237ee2eea08b406446c39a4486a993e540
conorkeane <g00391846@atu.ie>
2023-10-09 22:21:43 +0100

    Created Lab2 Exercise 3 Project

---
+++ src/main/java/ie/atu/lab2exercise3mac/lab2exercise3mac/Lab2Exercise3MacApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2exercise3mac.lab2exercise3mac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Exercise3MacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab2Exercise3MacApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/lab2exercise3mac/lab2exercise3mac/Lab2Exercise3MacApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2exercise3mac.lab2exercise3mac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Exercise3MacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
