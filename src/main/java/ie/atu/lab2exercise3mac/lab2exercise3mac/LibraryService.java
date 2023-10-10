62dc1eae145127cf36730da37dfb2786aa84573b
conorkeane <g00391846@atu.ie>
2023-10-10 19:09:39 +0100

    Added /addBook

--- src/main/java/ie/atu/lab2exercise3mac/lab2exercise3mac/LibraryService.java
+++ src/main/java/ie/atu/lab2exercise3mac/lab2exercise3mac/LibraryService.java
@@ -1,5 +1,16 @@
 package ie.atu.lab2exercise3mac.lab2exercise3mac;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
 public class LibraryService {
+
+    public void addBook(Book book) {
+        //Code to add new book
+    }
+
+    public void getBooks(Book book) {
+        //Code to get books
+    }
 
 }
