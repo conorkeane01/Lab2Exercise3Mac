4e47a14db573a6d216febefd1669f08107e33e69
conorkeane <g00391846@atu.ie>
2023-10-11 11:38:16 +0100

    Added Library feature to store books

--- src/main/java/ie/atu/lab2exercise3mac/lab2exercise3mac/LibraryService.java
+++ src/main/java/ie/atu/lab2exercise3mac/lab2exercise3mac/LibraryService.java
@@ -2,15 +2,22 @@ package ie.atu.lab2exercise3mac.lab2exercise3mac;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class LibraryService {
+    private List<Book> Library = new ArrayList<>();
 
     public void addBook(Book book) {
         //Code to add new book
+        Library.add(book);
     }
 
-    public void getBooks() {
+    public List<Book> getBooks() {
         //Code to get books
+        return Library;
     }
 
 }
